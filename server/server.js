4c6f71bd03dd699f516dd00523779f53556d8087
Faisal Xubayer <42772725+faisaljubayer@users.noreply.github.com>
2020-04-29 22:37:32 +0600

    Revert "(feat): created sample model"
    This reverts commit 711bbddb4a35f998d76e7184855ec278bc8cd2f1.

--- server/server.js
+++ server/server.js
@@ -1,32 +1,19 @@
 // server.js
 
 //Includes
-require('./src/db-connection.js');
+require('./src/db_connection.js');
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
 
 // Environment Variables
 dotenv.config(); 
 PORT = process.env.PORT;
 
 app.get('/', function(req,res){
-	res.send("Hello World. I am the king of the world !");
+	res.send("Hello World. I am here !");
 });
 
 app.listen(PORT, function(){
-	console.log("Server is listening on " + PORT);
+	console.log("Server Listening on " + PORT);
 });
-
-// Routes
-const postRouter = require('./src/routes/post.router');
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(bodyParser.json());
-
-app.use('/posts', postRouter);
