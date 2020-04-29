711bbddb4a35f998d76e7184855ec278bc8cd2f1
Faisal Xubayer <42772725+faisaljubayer@users.noreply.github.com>
2020-04-29 19:27:04 +0600

    (feat): created sample model

--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 // server.js
 
 //Includes
-require('./src/db_connection.js');
+require('./src/db-connection.js');
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 // Environment Variables
 dotenv.config(); 
 PORT = process.env.PORT;
 
 app.get('/', function(req,res){
-	res.send("Hello World. I am here !");
+	res.send("Hello World. I am the king of the world !");
 });
 
 app.listen(PORT, function(){
-	console.log("Server Listening on " + PORT);
+	console.log("Server is listening on " + PORT);
 });
+
+// Routes
+const postRouter = require('./src/routes/post.router');
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+
+app.use('/posts', postRouter);
