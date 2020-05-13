98937a10cae42f32a01f1166e353071d50427800
faisalakandha <57830958+faisalakandha@users.noreply.github.com>
2020-05-13 13:38:05 +0600

    (chore): mongoose more options created

--- server/index.js
+++ server/index.js
@@ -5,14 +5,19 @@ const cookieParser = require("cookie-parser");
 
 const config = require("./config/key");
 
-// const mongoose = require("mongoose");
-// mongoose
-//   .connect(config.mongoURI, { useNewUrlParser: true })
-//   .then(() => console.log("DB connected"))
-//   .catch(err => console.error(err));
+// mongoose options
+const options = {
+	useNewUrlParser: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+	autoIndex: false,
+	poolSize: 10,
+	bufferMaxEntries: 0
+};
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const connect = mongoose.connect(config.mongoURI, options)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
