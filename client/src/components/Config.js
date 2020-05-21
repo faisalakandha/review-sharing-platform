a58943cdf0453b21fda61db2044854ad1b1ba387
faisalakandha <faisalakandha24@gmail.com>
2020-05-21 23:01:07 +0600

    api configuration created

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,16 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+//API KEYS
+export const API_KEY = '5e89f882d394468b2d3a8b3b5746b111';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
+
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500'
