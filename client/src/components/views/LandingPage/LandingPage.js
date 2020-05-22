e7f11f46dec671d547041d986ad884f70af1d6fa
faisalakandha <faisalakandha24@gmail.com>
2020-05-22 15:28:07 +0600

    (feat.): Fetching from MovieDB API

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,16 @@
-import React from 'react'
-import { FaComment } from "react-icons/fa";
+import React,{useEffect} from 'react'
+import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY } from '../../Config'
+
+ useEffect(() => {
+       
+        fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+            .then(response=> response.json())
+            .then(response => {
+                console.log(response)
+            })
+
+    }, [])
 
 function LandingPage() {
     return (
